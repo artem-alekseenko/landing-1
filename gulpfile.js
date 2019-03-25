c49e43ba97fa0a5a07840d1dc03bc3b7f4a80d4f
Artem Alekseenko <ivanovo.cwi@gmail.com>
2019-03-25 17:20:41 +0300

    Update gulpfile.js and package.json. Add main.sccs and index.pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
+const spritesmith = require('gulp.spritesmith');
+const rimraf = require('rimraf');
+const rename = require('gulp-rename');
 
 /* -------- Server  -------- */
 gulp.task('server', function () {
